8098607fb97fbaccaa31e342da24498e56a5fe9a
Hasmik23 <ht.ohannesian@gmail.com>
2023-03-29 13:55:30 -0400

    git test

--- src/class09/GitCommit.java
+++ src/class09/GitCommit.java
@@ -3,7 +3,11 @@ package class09;
 public class GitCommit {
     public static void main(String[] args) {
 
-        System.out.println("my commit");
+        System.out.println("my commit 09");
+        System.out.println("my commit 09");
+        System.out.println("my commit 09");
+        System.out.println("my commit 09");
+        
 
     }
 }
